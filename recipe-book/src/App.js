4bb9287b42e7d74d8c1bd40c0fb7203cb44760c4
Emanuel <Emanuel.Voikhansky@gmail.com>
2021-02-10 15:26:30 -0800

    Added Flow static typechecking

--- recipe-book/src/App.js
+++ recipe-book/src/App.js
@@ -1,6 +1,24 @@
-import './App.css';
+// @flow
 
-function App() {
+import './App.css';
+import * as React from 'react';
+
+type Recipe = {
+  name: string,
+  stars: '1' | '2' | '3' | '4' | '5',
+  imageUrl?: string,
+  instructions: string,
+};
+
+const HARDCODED_DEV_DATA: Array<Recipe> = [
+  {
+    name: "Roast Beef",
+    stars: '4',
+    instructions: "How to cook roast beef...."
+  }
+];
+
+function App(): React.Node {
   return (
     <div className="App">
       Placeholder, will start writing UI in next commit
